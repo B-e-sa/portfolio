688abb2ef9b5afa7b39f15c8491b4c21ae21b6cc
B3_S4 <joaoantonio17@yahoo.com.br>
2022-08-12 01:34:04 -0300

    add conditional rendering

--- src/components/home/MyWork/MyWork.tsx
+++ src/components/home/MyWork/MyWork.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Carousel from './Carousel'
 import Works from './Works'
 import './MyWork.css'
@@ -8,10 +9,28 @@ import portfolio from '../../../assets/projects/portfolio.png'
 const cards: string[] = [`${weather}`, `${pokedex}`, `${portfolio}`]
 
 const MyWork = () => {
+
+    const [layoutWidth, setLayoutWidth] = useState(window.innerWidth > 730)
+
+    const updateLayout = (): void => {
+        setLayoutWidth(window.innerWidth > 730);
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", updateLayout)
+        return () => window.removeEventListener("resize", updateLayout);
+    }, [])
+
     return (
-        <div id='my-work'>
-            <Carousel cards={cards} />
-            <Works />
+        <div id='my work'>
+            {layoutWidth ?
+                <div>
+                    <Carousel cards={cards} />
+                </div>
+                :
+                <div>
+                    <Works />
+                </div>}
         </div>
     )
 }
