8c1b46a044a3fd20a67e109d81538a8c09da7724
João Antônio <101114950+B-e-sa@users.noreply.github.com>
2022-08-24 16:54:35 -0300

    Adding light mode to dev-cat

--- src/components/home/AboutMe/AboutMe.tsx
+++ src/components/home/AboutMe/AboutMe.tsx
@@ -1,13 +1,18 @@
 import './AboutMe.css'
 import devCat from '../../../assets/cats/dev-cat.png'
 import ligth from '../../../assets/cats/light.png'
+import { useContext } from 'react'
+import { ThemeContext } from '../../../pages/Home'
 
 const AboutMe = () => {
+
+    const { theme } = useContext(ThemeContext) 
+
     return (
         <div id='about-me-container'>
             <hr />
             <div id='about-me-content'>
-                <div id='about-me-image-container'>
+                <div id='about-me-image-container' style={{filter: theme === 'light' ? "invert(100%)" : "invert(0%)"}}>
                     <img id='light' src={ligth} alt="light-bulb" className='non-draggable'/>
                     <img id='about-me-image' src={devCat} alt="dev-cat" className='non-draggable'/></div>
                 <div id='about-info'>
